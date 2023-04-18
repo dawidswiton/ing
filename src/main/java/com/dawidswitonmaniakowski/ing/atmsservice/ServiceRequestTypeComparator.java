b0b893a1cf1096d3fa999fc35b51be54bc2702d9
Dawid Świtoń-Maniakowski <dawid.switon@gmail.com>
2023-04-18 11:05:59 +0200

    fix spot bug serialization interface

--- src/main/java/com/dawidswitonmaniakowski/ing/atmsservice/ServiceRequestTypeComparator.java
+++ src/main/java/com/dawidswitonmaniakowski/ing/atmsservice/ServiceRequestTypeComparator.java
@@ -1,8 +1,13 @@
 package com.dawidswitonmaniakowski.ing.atmsservice;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Comparator;
 
-class ServiceRequestTypeComparator implements Comparator<ServiceRequestType> {
+class ServiceRequestTypeComparator implements Comparator<ServiceRequestType>, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1000L;
 
     public int compare(ServiceRequestType o1, ServiceRequestType o2) {
         return Integer.compare(o1.getPriority(), o2.getPriority());
